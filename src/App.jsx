1098874f9733787ad265a9c97a7a8f8a91ac7436
Шепыч <syndicating@yandex.ru>
2024-06-20 20:20:43 +0300

    Feat: сортировка + условие на отображение списка

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
-import CardButton from './components/CardButton/CardButton';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
-import JournalItem from './components/JournalItem/JournalItem';
 import JournalList from './components/JournalList/JournalList';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 import Body from './layouts/Body/Body';
@@ -10,21 +8,24 @@ import JournalForm from './components/JournalForm/JournalForm';
 import { useState } from 'react';
 
 const INITIAL_DATA = [
-	{
-		title: 'Подготовка к обновлению курсов',
-		date: new Date(),
-		text: 'Сегодня провёл весь день за...'
-	},
-	{
-		title: 'Поход в годы',
-		date: new Date(),
-		text: 'Думал, что очень много време...'
-	},
-	{
-		title: 'Первая заметка',
-		date: new Date(),
-		text: 'Создал первую заметку, чтобы ...'
-	}
+	// {
+	// 	id: 1,
+	// 	title: 'Подготовка к обновлению курсов',
+	// 	date: new Date(),
+	// 	text: 'Сегодня провёл весь день за...'
+	// },
+	// {
+	// 	id: 2,
+	// 	title: 'Поход в годы',
+	// 	date: new Date(),
+	// 	text: 'Думал, что очень много време...'
+	// },
+	// {
+	// 	id: 3,
+	// 	title: 'Первая заметка',
+	// 	date: new Date(),
+	// 	text: 'Создал первую заметку, чтобы ...'
+	// }
 ];
 
 function App() {
@@ -34,7 +35,8 @@ function App() {
 		setItems(oldItems => [...oldItems, {
 			title: item.title,
 			text: item.text,
-			date: new Date(item.date)
+			date: new Date(item.date),
+			id: Math.max(...oldItems.map(i => i.id)) + 1
 		}]);
 	};
 
@@ -42,20 +44,10 @@ function App() {
 		<>
 			<LeftPanel>
 				<Header/>
-				<JournalList>
-					<JournalAddButton>
-						Новое воспоминание
-					</JournalAddButton>
-					{items.map(el => (
-						<CardButton>
-							<JournalItem 
-								title={el.title}
-								date={el.date}
-								text={el.text}
-							/>
-						</CardButton>
-					))}
-				</JournalList>
+				<JournalAddButton>
+					Новое воспоминание
+				</JournalAddButton>
+				<JournalList items={items}/>
 			</LeftPanel>
 			<Body>
 				<JournalForm onSubmit={addItem}/>
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,11 +1,40 @@
 import './JournalList.css';
+import CardButton from '../CardButton/CardButton';
+import JournalItem from '../JournalItem/JournalItem';
 
-function JournalList({ children }) {
-	return (
-		<div className='journal-list'>
-			{ children }
-		</div>
-	);
+function JournalList({ items }) {
+
+	if(items.length === 0) {
+		return <p>Заметки отсутствуют</p>;
+	}
+
+	const sortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	return <div className='journal-list'>{
+		items.sort(sortItems).map(el => (
+			
+				<CardButton key={el.id}>
+					<JournalItem 
+						title={el.title}
+						date={el.date}
+						text={el.text}
+					/>
+				</CardButton>
+			
+		))
+	}
+	</div>
+	// return (
+	// 	<div className='journal-list'>
+	// 		{ children }
+	// 	</div>
+	// );
 }
 
 export default JournalList;
